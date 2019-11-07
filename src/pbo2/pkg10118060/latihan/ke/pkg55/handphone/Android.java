734e95d01cd4b18ffb39fac9f567c18fbef3135a
Synxsaints <Synxsaints@LAPTOP-EAMMNJ87>
2019-11-07 21:04:48 +0700

    First Commit

---
+++ src/pbo2/pkg10118060/latihan/ke/pkg55/handphone/Android.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118060.latihan.ke.pkg55.handphone;
+
+/**
+ *
+ * @author Synxsaints
+ */
+public class Android extends Handphone {
+    private String keyStore;
+
+    public Android(String man, String os, String model, int harga) {
+        super(man, os, model, harga);
+    }
+
+    public String getKeyStore() {
+        return keyStore;
+    }
+
+    public void setKeyStore(String keyStore) {
+        this.keyStore = keyStore;
+    }
+    
+}
